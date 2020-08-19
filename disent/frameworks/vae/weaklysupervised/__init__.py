67e1e821958e519a12967f468dd8387f5cfcf36e
Nathan Michlo <NathanJMichlo@gmail.com>
2020-08-19 13:28:21 +0200

    st-adavae

--- disent/frameworks/vae/weaklysupervised/__init__.py
+++ disent/frameworks/vae/weaklysupervised/__init__.py
@@ -1,2 +1,3 @@
 from disent.frameworks.vae.weaklysupervised._adavae import AdaVae
 from disent.frameworks.vae.weaklysupervised._st_betavae import SwappedTargetBetaVae
+from disent.frameworks.vae.weaklysupervised._st_adavae import SwappedTargetAdaVae
