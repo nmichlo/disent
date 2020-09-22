5b1576349fbe1d687faf10d6e2a4922634add88e
Nathan Michlo <NathanJMichlo@gmail.com>
2020-09-22 15:21:33 +0200

    deleted dataset transforms folder

--- disent/frameworks/_in_progress/simclr.py
+++ disent/frameworks/_in_progress/simclr.py
@@ -1,9 +1,8 @@
-from typing import Any
-
+import cv2
 import torch
+import numpy as np
 import torchvision
 from pl_bolts.models.self_supervised import SimCLR
-from disent.dataset.transforms import GaussianBlurTransform
 from disent.frameworks.framework import BaseFramework
 
 
@@ -65,6 +64,25 @@ def make_simclr_transforms(size, s=1):
             torchvision.transforms.ToTensor(),
     ])
 
+
+class GaussianBlurTransform(object):
+    """
+    Gaussian blur as described in the SimCLR paper (without the random application chance).
+    - had random chance to choose blur radius
+    - https://github.com/sthalles/SimCLR
+    """
+
+    def __init__(self, kernel_size, sigma_min=0.1, sigma_max=2.0):
+        self.kernel_size = kernel_size
+        self._sigma_min = sigma_min
+        self._sigma_max = sigma_max
+
+    def __call__(self, sample):
+        # TODO: fix casting. Is this actually needed?
+        # sample = np.array(sample)
+        assert sample.dtype == np.float32  # or sample.dtype == np.float16
+        sigma = (self._sigma_max - self._sigma_min) * np.random.random_sample() + self._sigma_min
+        return cv2.GaussianBlur(sample, (self.kernel_size, self.kernel_size), sigma)
 
 # ========================================================================= #
 # END                                                                       #
