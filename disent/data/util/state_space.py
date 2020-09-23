9eff975ca515e50539174e05f4feef863521da98
Nathan Michlo <NathanJMichlo@gmail.com>
2020-09-23 04:26:17 +0200

    StateSpace rewrite - now fully supports arbitrary dimensions

--- disent/data/util/state_space.py
+++ disent/data/util/state_space.py
@@ -22,7 +22,7 @@ class _BaseStateSpace(LengthIter):
     def factor_sizes(self):
         """A list of sizes or dimensionality of factors handled by this state space"""
         raise NotImplementedError
-    
+
     def __len__(self):
         """Same as self.size"""
         return self.size
@@ -46,20 +46,25 @@ class _BaseStateSpace(LengthIter):
         - positions are lists of integers, with each element < their corresponding factor size
         """
         raise NotImplementedError
-    
-    def sample_factors(self, num_samples, factor_indices=None):
+
+    def sample_factors(self, num_samples=None, factor_indices=None):
         """
         sample randomly from all factors, otherwise the given factor_indices.
         returned values must appear in the same order as factor_indices.
+
+        If factor factor_indices is None, all factors are sampled.
+        If num_samples=None then the array returned is the same shape as factor_sizes[factor_indices]
+        If num_samples is an integer or shape, the samples returned are that shape with the last dimension
+            the same size as factor_indices, ie (*num_samples, len(factor_indices))
         """
         raise NotImplementedError
 
-    def sample_missing_factors(self, partial_factors, partial_factor_indices):
+    def sample_missing_factors(self, known_factors, known_factor_indices):
         """
-        Samples the remaining factors not given in the partial_factor_indices.
+        Samples the remaining factors not given in the known_factor_indices.
         ie. fills in the missing values by sampling from the unused dimensions.
         returned values are ordered by increasing factor index and not factor_indices.
-        (partial_factors must correspond to partial_factor_indices)
+        (known_factors must correspond to known_factor_indices)
         """
         raise NotImplementedError
 
@@ -67,6 +72,21 @@ class _BaseStateSpace(LengthIter):
         """
         Resample across all the factors, keeping factor_indices constant.
         returned values are ordered by increasing factor index and not factor_indices.
+        """
+        raise NotImplementedError
+
+    def resample_radius(self, factors, resample_radius=None, distinct=True, num_shared_factors=None, shared_factor_indices=None):
+        """
+        • If resample_radius is specified resamples along all factor dimensions with
+            but within the specified radius:
+            (factors - resample_radius) <= resampled_factors <= (factors + resample_radius)
+        • If distinct is specified then new factors cannot equal the original factors.
+            factors != resampled_factors
+        • If num_shared_factors is specified, then this many factors of the original are
+            preserved.
+        • If shared_factor_indices is specified then shared factors are limited to these
+            indices, however, num_shared_factors still takes priority. Thus make sure
+            len(shared_factor_indices) >= num_shared_factors
         """
         raise NotImplementedError
 
@@ -103,7 +123,11 @@ class StateSpace(_BaseStateSpace):
     @property
     def factor_sizes(self):
         return self._factor_sizes
-    
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+    # Coordinate Transform - any dim array, only last axis counts!          #
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+
     def pos_to_idx(self, positions):
         positions = np.array(positions).T
         return np.ravel_multi_index(positions, self._factor_sizes)
@@ -112,27 +136,60 @@ class StateSpace(_BaseStateSpace):
         positions = np.unravel_index(indices, self._factor_sizes)
         return np.array(positions).T
 
-    def sample_factors(self, num_samples, factor_indices=None):
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+    # Sampling Functions - any dim array, only last axis counts!            #
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+
+    def sample_factors(self, size=None, factor_indices=None):
+        # get factor sizes
         sizes = self._factor_sizes if (factor_indices is None) else self._factor_sizes[factor_indices]
-        return np.random.randint(sizes, size=(num_samples, len(sizes)))
+        # get resample size
+        if size is not None:
+            # empty np.array(()) gets dtype float which is incompatible with len
+            size = np.append(np.array(size, dtype=int), len(sizes))
+        # sample for factors
+        return np.random.randint(0, sizes, size=size)
 
     def sample_missing_factors(self, known_factors, known_factor_indices):
-        num_samples, num_dims = known_factors.shape
-        used_indices_set = set(known_factor_indices)
-        # assertions
-        assert num_dims == len(known_factor_indices), 'dimension count mismatch'
-        assert len(used_indices_set) == len(known_factor_indices), 'factor indices are repeated'
-        # set used dimensions
-        all_values = np.zeros(shape=(num_samples, self.num_factors), dtype=np.int64)
-        all_values[:, known_factor_indices] = known_factors
-        # sample for missing
-        missing_indices = list(self._factor_indices_set - used_indices_set)
-        all_values[:, missing_indices] = self.sample_factors(num_samples=num_samples, factor_indices=missing_indices)
-        # return
-        return all_values
+        known_factors = np.array(known_factors)
+        known_mask = np.zeros(self.num_factors, dtype='bool')
+        known_mask[known_factor_indices] = True
+        all_factors = np.zeros((*known_factors.shape[:-1], self.num_factors), dtype='int')
+        all_factors[..., known_mask] = known_factors
+        all_factors[..., ~known_mask] = self.sample_factors(size=known_factors.shape[:-1], factor_indices=~known_mask)
+        return all_factors
 
     def resample_factors(self, factors, fixed_factor_indices):
-        return self.sample_missing_factors(factors[:, fixed_factor_indices], fixed_factor_indices)
+        return self.sample_missing_factors(np.array(factors)[..., fixed_factor_indices], fixed_factor_indices)
+
+    def resample_radius(self, factors, resample_radius=None, distinct=True, num_shared_factors=None, shared_factor_indices=None):
+        factors = np.array(factors)
+
+        # RESAMPLE ALL:
+        # ~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~ #
+        # sample along entire factor ranges
+        factors_min = 0
+        factors_max = np.array(self.factor_sizes) - 1
+        # sample in radius along factors with no wraparound
+        if resample_radius is not None:
+            factors_min = np.maximum(factors - resample_radius, factors_min)
+            factors_max = np.minimum(factors + resample_radius, factors_max)
+        # create new sample
+        resampled_factors = np.random.randint(factors_min, factors_max if distinct else (factors_max+1), size=factors.shape)
+        # exclude the original values from factors
+        if distinct:
+            resampled_factors += (factors <= resampled_factors)  # shift by +1 if >= to original factors
+
+        # KEEP SHARED:
+        # ~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~ #
+        if num_shared_factors is not None:
+            choices = self.num_factors if (shared_factor_indices is None) else shared_factor_indices
+            shared_factor_indices = np.random.choice(choices, size=num_shared_factors, replace=False)
+        if shared_factor_indices is not None:
+            resampled_factors[..., shared_factor_indices] = factors[..., shared_factor_indices]
+        # ~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~ #
+
+        return resampled_factors
 
 
 # ========================================================================= #
@@ -141,16 +198,18 @@ class StateSpace(_BaseStateSpace):
 
 
 class HiddenStateSpace(_BaseStateSpace):
-    
+
     """
     State space where an index corresponds to coordinates (factors/positions) in the factor space.
     HOWEVER: some factors are treated as hidden/unknown and are thus randomly sampled.
-    
+
     Inputs to functions act as if known_factor_indices is the new factor space (including factor indexes).
     Outputs from functions act in the full factor space.
     """
-    
-    def __init__(self, factor_sizes, known_factor_indices):
+
+    def __init__(self, factor_sizes, known_factor_indices=None):
+        if known_factor_indices is None:
+            known_factor_indices = np.arange(len(factor_sizes))
         factor_sizes, known_factor_indices = np.array(factor_sizes), np.array(known_factor_indices)
         # known factors indices
         self._known_factor_indices = known_factor_indices
@@ -164,46 +223,40 @@ class HiddenStateSpace(_BaseStateSpace):
     @property
     def size(self):
         return self._known_state_space.size
-    
+
     @property
     def num_factors(self):
         return self._known_state_space.num_factors
-    
+
     @property
     def factor_sizes(self):
         return self._known_state_space.factor_sizes
-    
+
     def pos_to_idx(self, positions):
         positions = np.array(positions)
-        if positions.ndim == 1:
-            return self._known_state_space.pos_to_idx(positions[self._known_factor_indices])
-        elif positions.ndim == 2:
-            return self._known_state_space.pos_to_idx(positions[:, self._known_factor_indices])
-        else:
-            raise IndexError(f'positions has incorrect number of dimensions: {positions.ndim}')
+        assert 1 <= positions.ndim <= 2, f'positions has incorrect number of dimensions: {positions.ndim}'
+        assert positions.shape[-1] == self._full_state_space.num_factors, 'last dimension of positions must equal the full state space size'
+        # remove the unknown dimensions and return the index
+        return self._known_state_space.pos_to_idx(positions[..., self._known_factor_indices])
 
     def idx_to_pos(self, indices):
         indices = np.array(indices)
-        if indices.ndim == 0:
-            known_factors = self._known_state_space.idx_to_pos(indices.reshape(-1))
-            sampled_factors = self._full_state_space.sample_missing_factors(known_factors, self._known_factor_indices)[0]
-        elif indices.ndim == 1:
-            known_factors = self._known_state_space.idx_to_pos(indices)
-            sampled_factors = self._full_state_space.sample_missing_factors(known_factors, self._known_factor_indices)
-        else:
-            raise IndexError(f'indices has incorrect number of dimensions: {indices.ndim}')
-        return sampled_factors
-        
-    def sample_factors(self, num_samples, factor_indices=None):
+        assert 0 <= indices.ndim <= 1, f'indices has incorrect number of dimensions: {indices.ndim}'
+        # get factors and return
+        known_factors = self._known_state_space.idx_to_pos(indices.reshape(-1))
+        sampled_factors = self._full_state_space.sample_missing_factors(known_factors, self._known_factor_indices)
+        return sampled_factors.reshape((*indices.shape, self._full_state_space.num_factors))
+
+    def sample_factors(self, size=None, factor_indices=None):
         return self._full_state_space.sample_factors(
-            num_samples,
+            size,
             self._known_factor_indices[factor_indices] if factor_indices else None
         )
-    
-    def sample_missing_factors(self, partial_factors, partial_factor_indices):
+
+    def sample_missing_factors(self, known_factors, known_factor_indices):
         return self._full_state_space.sample_missing_factors(
-            partial_factors,
-            self._known_factor_indices[partial_factor_indices]
+            known_factors,
+            self._known_factor_indices[known_factor_indices]
         )
 
     def resample_factors(self, factors, fixed_factor_indices):
@@ -220,7 +273,7 @@ class HiddenStateSpace(_BaseStateSpace):
 
 class StateSpaceRemapIndex(object):
     """Mapping from incorrectly ordered factors to state space indices"""
-    
+
     def __init__(self, factor_sizes, features):
         self._states = StateSpace(factor_sizes)
         # get indices of features
@@ -232,7 +285,7 @@ class StateSpaceRemapIndex(object):
         # mapping
         self._state_to_orig_idx = np.zeros(self._states.size, dtype=np.int64)
         self._state_to_orig_idx[orig_indices] = state_indices
-    
+
     def factors_to_orig_idx(self, factors):
         """
         get the original index of factors
