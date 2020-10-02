dec85cda8a1517145efaa86143e1657c17c548b9
Nathan Michlo <NathanJMichlo@gmail.com>
2020-10-02 14:38:08 +0200

    triplet fixes

--- disent/frameworks/vae/supervised/_tgadavae.py
+++ disent/frameworks/vae/supervised/_tgadavae.py
@@ -1,6 +1,6 @@
 from disent.frameworks.vae.supervised import GuidedAdaVae
-import torch
-import torch.nn.functional as F
+from disent.frameworks.vae.supervised._tvae import augment_loss_triplet
+
 
 # ========================================================================= #
 # tgadavae                                                                  #
@@ -25,31 +25,12 @@ class TripletGuidedAdaVae(GuidedAdaVae):
         self.triplet_scale = triplet_scale
 
     def augment_loss(self, a_z_mean, a_z_logvar, p_z_mean, p_z_logvar, n_z_mean, n_z_logvar):
-        loss_triplet = triplet_loss(a_z_mean, p_z_mean, n_z_mean, margin=self.triplet_margin)
-        augmented_loss = self.triplet_scale * loss_triplet
-        return augmented_loss, {
-            'triplet_loss': loss_triplet,
-            'triplet_loss_torch': F.triplet_margin_loss(a_z_mean, p_z_mean, n_z_mean, margin=self.triplet_margin)
-        }
-
-
-# ========================================================================= #
-# TRIPLET LOSS                                                              #
-# ========================================================================= #
-
-
-def triplet_loss(anchor, positive, negative, margin=0.3):
-    # import tensorflow as tf
-    # positive_dist = tf.reduce_sum(tf.square(tf.subtract(anchor, positive)), 1)
-    # negative_dist = tf.reduce_sum(tf.square(tf.subtract(anchor, negative)), 1)
-    # loss_1 = tf.add(tf.subtract(positive_dist, negative_dist), alpha)
-    # loss = tf.reduce_sum(tf.maximum(loss_1, 0.0), 0)
-
-    positive_dist = torch.sum((anchor - positive)**2, dim=1)
-    negative_dist = torch.sum((anchor - negative)**2, dim=1)
-    clamped = torch.clamp_min((positive_dist - negative_dist) + margin, 0)
-    loss = torch.mean(clamped, dim=0)  # TODO: this was sum
-    return loss
+        return augment_loss_triplet(
+            a_z_mean, a_z_logvar,
+            p_z_mean, p_z_logvar,
+            n_z_mean, n_z_logvar,
+            scale=self.triplet_scale, margin=self.triplet_margin
+        )
 
 
 # ========================================================================= #
