48fa8dc130e4386a6815597f5eb9446382d4b95a
Nathan Michlo <NathanJMichlo@gmail.com>
2020-10-19 17:41:18 +0200

    split out common from base models

--- disent/model/base.py
+++ disent/model/base.py
@@ -1,47 +1,12 @@
 import logging
+from typing import final
 import numpy as np
-import torch.nn as nn
 from torch import Tensor
 
+from disent.util import DisentModule
+
+
 log = logging.getLogger(__name__)
-
-# ========================================================================= #
-# Utility Layers                                                            #
-# ========================================================================= #
-
-class Print(nn.Module):
-    """From: https://github.com/1Konny/Beta-VAE/blob/master/model.py"""
-    def __init__(self, layer):
-        super().__init__()
-        self.layer = layer
-
-    def forward(self, tensor):
-        log.debug(self.layer, '|', tensor.shape, '->')
-        output = self.layer.forward(tensor)
-        log.debug(output.shape)
-        return output
-
-class BatchView(nn.Module):
-    """From: https://github.com/1Konny/Beta-VAE/blob/master/model.py"""
-    def __init__(self, size):
-        super().__init__()
-        self.size = (-1, *size)
-
-    def forward(self, tensor):
-        return tensor.view(*self.size)
-
-class Unsqueeze3D(nn.Module):
-    """From: https://github.com/amir-abdi/disentanglement-pytorch"""
-    def forward(self, x):
-        x = x.unsqueeze(-1)
-        x = x.unsqueeze(-1)
-        return x
-
-class Flatten3D(nn.Module):
-    """From: https://github.com/amir-abdi/disentanglement-pytorch"""
-    def forward(self, x):
-        x = x.view(x.size()[0], -1)
-        return x
 
 
 # ========================================================================= #
