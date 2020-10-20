6cff8e0348503ba036030a2e54abec8f6d3c90be
Nathan Michlo <NathanJMichlo@gmail.com>
2020-10-20 14:05:29 +0200

    Original Weak Sampling For Dataset Pairs

--- disent/dataset/groundtruth/__init__.py
+++ disent/dataset/groundtruth/__init__.py
@@ -1,3 +1,4 @@
 from ._single import GroundTruthDataset
 from ._pair import GroundTruthDatasetPairs
+from ._pair_weak import GroundTruthDatasetOrigWeakPairs
 from ._triplet import GroundTruthDatasetTriples
