dd364e95700588c0e1cf00d7b482e8e21f59b429
Nathan Michlo <NathanJMichlo@gmail.com>
2021-02-25 14:01:04 +0200

    removed BetaVaeH

--- disent/frameworks/vae/unsupervised/__init__.py
+++ disent/frameworks/vae/unsupervised/__init__.py
@@ -24,7 +24,6 @@
 
 from disent.frameworks.vae.unsupervised._vae import Vae
 from disent.frameworks.vae.unsupervised._betavae import BetaVae
-from disent.frameworks.vae.unsupervised._betavae import BetaVaeH
 from disent.frameworks.vae.unsupervised._dfcvae import DfcVae
 
 # experimental frameworks
