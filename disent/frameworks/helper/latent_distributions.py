average loss helper functions + fixes to dfcvae, betavae, dipvae

disent/frameworks/helper/latent_distributions.py
@@ -32,6 +32,7 @@ import torch
 from torch.distributions import Normal, Distribution
 
 from disent.frameworks.helper.reductions import loss_reduction
+from disent.frameworks.helper.util import compute_ave_loss
 from disent.util import TupleDataClass
 
 
@@ -133,11 +134,7 @@ class LatentDistsHandler(object):
 
     @final
     def compute_ave_kl_loss(self, ds_posterior: Sequence[Distribution], ds_prior: Sequence[Distribution], zs_sampled: Sequence[torch.Tensor]) -> torch.Tensor:
-        assert len(ds_posterior) == len(ds_prior) == len(zs_sampled)
-        return torch.stack([
-            self.compute_kl_loss(posterior, prior, z_sampled)
-            for posterior, prior, z_sampled in zip(ds_posterior, ds_prior, zs_sampled)
-        ]).mean(dim=0)
+        return compute_ave_loss(self.compute_kl_loss, ds_posterior, ds_prior, zs_sampled)
 
 
 # ========================================================================= #

disent/frameworks/helper/reconstructions.py
@@ -30,6 +30,7 @@ import torch
 import torch.nn.functional as F
 
 from disent.frameworks.helper.reductions import loss_reduction
+from disent.frameworks.helper.util import compute_ave_loss
 
 
 # ========================================================================= #
@@ -49,6 +50,9 @@ class ReconLossHandler(object):
         """
         raise NotImplementedError
 
+    def activate_all(self, xs):
+        return [self.activate(x) for x in xs]
+
     @final
     def compute_loss(self, x_partial_recon: torch.Tensor, x_targ: torch.Tensor) -> torch.Tensor:
         """
@@ -63,11 +67,7 @@ class ReconLossHandler(object):
 
     @final
     def compute_ave_loss(self, xs_partial_recon: Sequence[torch.Tensor], xs_targ: Sequence[torch.Tensor]) -> torch.Tensor:
-        assert len(xs_partial_recon) == len(xs_targ)
-        return torch.stack([
-            self.compute_loss(x_partial_recon, x_targ)
-            for x_partial_recon, x_targ in zip(xs_partial_recon, xs_targ)
-        ]).mean(dim=0)
+        return compute_ave_loss(self.compute_loss, xs_partial_recon, xs_targ)
 
     def _compute_unreduced_loss(self, x_partial_recon: torch.Tensor, x_targ: torch.Tensor) -> torch.Tensor:
         """

disent/frameworks/vae/unsupervised/_betavae.py
@@ -23,8 +23,13 @@
 #  ~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~
 
 from dataclasses import dataclass
+from numbers import Number
+from typing import Any
 from typing import Dict
 from typing import Optional
+from typing import Sequence
+from typing import Tuple
+from typing import Union
 
 import torch
 from torch.distributions import Distribution
@@ -71,9 +76,9 @@ class BetaVae(Vae):
     # Overrides                                                             #
     # --------------------------------------------------------------------- #
 
-    def compute_reg_loss(self, d_posterior: Distribution, d_prior: Distribution, z_sampled: Optional[torch.Tensor]) -> (torch.Tensor, Dict[str, float]):
-        # BetaVAE - compute regularization loss
-        kl_loss = self.latents_handler.compute_kl_loss(d_posterior, d_prior, z_sampled)
+    def compute_ave_reg_loss(self, ds_posterior: Sequence[Distribution], ds_prior: Sequence[Distribution], zs_sampled: Sequence[torch.Tensor]) -> Tuple[Union[torch.Tensor, Number], Dict[str, Any]]:
+        # BetaVAE: compute regularization loss (kl divergence)
+        kl_loss = self.latents_handler.compute_ave_kl_loss(ds_posterior, ds_prior, zs_sampled)
         kl_reg_loss = self.cfg.beta * kl_loss
         # return logs
         return kl_reg_loss, {

disent/frameworks/vae/unsupervised/_dfcvae.py
@@ -37,6 +37,7 @@ from torchvision.models import vgg19_bn
 from torch.nn import functional as F
 
 from disent.frameworks.helper.reductions import get_mean_loss_scale
+from disent.frameworks.helper.util import compute_ave_loss
 from disent.frameworks.vae.unsupervised import BetaVae
 from disent.transform.functional import check_tensor
 
@@ -80,10 +81,8 @@ class DfcVae(BetaVae):
         # compute ave reconstruction loss
         pixel_loss = self.recon_handler.compute_ave_loss(xs_partial_recon, xs_targ)  # (DIFFERENCE: 1)
         # compute ave deep features loss
-        feature_loss = torch.stack([
-            self._dfc_loss.compute_loss(self.recon_handler.activate(x_partial_recon), x_targ, reduction=self.cfg.loss_reduction)
-            for x_partial_recon, x_targ in zip(xs_partial_recon, xs_targ)
-        ]).mean(dim=-1)
+        xs_recon = self.recon_handler.activate_all(xs_partial_recon)
+        feature_loss = compute_ave_loss(self._dfc_loss.compute_loss, xs_recon, xs_targ, reduction=self.cfg.loss_reduction)
         # reconstruction error
         # TODO: not in reference implementation, but terms should be weighted
         # TODO: not in reference but feature loss is not scaled properly
@@ -110,6 +109,8 @@ class DfcLossModule(torch.nn.Module):
     Difference:
     - normalise data as torchvision.models require.
     """
+
+    # TODO: this should be converted to a reconstruction loss handler
 
     def __init__(self, feature_layers: Optional[List[Union[str, int]]] = None):
         """

disent/frameworks/vae/unsupervised/_dipvae.py
@@ -25,10 +25,12 @@
 from dataclasses import dataclass
 from typing import Dict
 from typing import Optional
+from typing import Sequence
 
 import torch
 from torch.distributions import Normal
 
+from disent.frameworks.helper.util import compute_ave_loss_and_logs
 from disent.frameworks.vae.unsupervised import BetaVae
 
 
@@ -66,17 +68,17 @@ class DipVae(BetaVae):
     # Overrides                                                             #
     # --------------------------------------------------------------------- #
 
-    def compute_reg_loss(self, d_posterior: Normal, d_prior: Normal, z_sampled: Optional[torch.Tensor]) -> (torch.Tensor, Dict[str, float]):
+    def compute_ave_reg_loss(self, ds_posterior: Sequence[Normal], ds_prior: Sequence[Normal], zs_sampled):
         # compute kl loss
-        kl_reg_loss, logs_kl_reg = super().compute_reg_loss(d_posterior, d_prior, z_sampled)
+        kl_reg_loss, logs_kl_reg = super().compute_ave_reg_loss(ds_posterior, ds_prior, zs_sampled)
         # compute dip loss
-        dip_reg_loss, logs_dip_reg = self._dip_compute_loss(d_posterior)
+        dip_reg_loss, logs_dip_reg = compute_ave_loss_and_logs(self._dip_compute_loss, ds_posterior)
         # combine
         combined_loss = kl_reg_loss + dip_reg_loss
         # return logs
         return combined_loss, {
-            *logs_kl_reg,  # kl_loss, kl_reg_loss
-            *logs_dip_reg,  # dip_loss_d, dip_loss_od, dip_loss, dip_reg_loss,
+            **logs_kl_reg,   # kl_loss, kl_reg_loss
+            **logs_dip_reg,  # dip_loss_d, dip_loss_od, dip_loss, dip_reg_loss,
         }
 
     # --------------------------------------------------------------------- #