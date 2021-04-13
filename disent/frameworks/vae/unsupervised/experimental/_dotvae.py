379070a5ec6d15b32132dc958f45284f15e46b87
Nathan Michlo <NathanJMichlo@gmail.com>
2021-04-13 18:23:42 +0200

    re-write dotvae

--- disent/frameworks/vae/unsupervised/experimental/_dotvae.py
+++ disent/frameworks/vae/unsupervised/experimental/_dotvae.py
@@ -72,38 +72,38 @@ class DataOverlapTripletVae(AdaNegTripletVae):
             self._augment = instantiate_recursive(self.cfg.overlap_augment)
 
     def hook_compute_ave_aug_loss(self, ds_posterior: Sequence[Normal], ds_prior, zs_sampled, xs_partial_recon, xs_targ: Sequence[torch.Tensor]):
-        # get values
-        (d_posterior,), (x_targ,) = ds_posterior, xs_targ
-        # generate & mine random triples from batch -- this does not generate unique pairs
-        a_idxs, p_idxs, n_idxs = torch.randint(len(x_targ), size=(3, min(self.cfg.overlap_num, len(x_targ)**3)))
-        a_idxs, p_idxs, n_idxs = self.mine_triplets(x_targ, a_idxs, p_idxs, n_idxs)
-        # make triples
-        new_xs_targ = [x_targ[idxs] for idxs in (a_idxs, p_idxs, n_idxs)]
-        new_ds_posterior = [Normal(d_posterior.loc[idxs], d_posterior.scale[idxs]) for idxs in (a_idxs, p_idxs, n_idxs)]
-        # augment targets
+        # ++++++++++++++++++++++++++++++++++++++++++ #
+        # 1. augment batch
         with torch.no_grad():
-            aug_xs_targ = self.augment_triplet_targets(new_xs_targ)
-        # create new triplets
-        ds_posterior_NEW = self.overlap_create_triplets(ds_posterior=new_ds_posterior, xs_targ=aug_xs_targ, cfg=self.cfg, unreduced_loss_fn=self.recon_handler.compute_unreduced_loss)
-        # compute loss
-        loss, logs = AdaNegTripletVae.estimate_ada_triplet_loss(
-            ds_posterior=ds_posterior_NEW,
+            xs_targ = self.augment_triplet_targets(xs_targ)
+        (d_posterior,), (x_targ,) = ds_posterior, xs_targ
+        # 2. generate random triples -- this does not generate unique pairs
+        a_idxs, p_idxs, n_idxs = torch.randint(len(x_targ), size=(3, min(self.cfg.overlap_num, len(x_targ)**3)), device=x_targ.device)
+        # 3. reorder random triples
+        a_idxs, p_idxs, n_idxs = self.overlap_swap_triplet_idxs(x_targ, a_idxs, p_idxs, n_idxs, cfg=self.cfg, unreduced_loss_fn=self.recon_handler.compute_unreduced_loss)
+        # 4. mine random triples
+        a_idxs, p_idxs, n_idxs = self.mine_triplets(x_targ, a_idxs, p_idxs, n_idxs)
+        # ++++++++++++++++++++++++++++++++++++++++++ #
+        # 5. compute triplet loss
+        return AdaNegTripletVae.estimate_ada_triplet_loss(
+            ds_posterior=[Normal(d_posterior.loc[idxs], d_posterior.scale[idxs]) for idxs in (a_idxs, p_idxs, n_idxs)],
             cfg=self.cfg,
         )
-        return loss, {
-            **logs,
-            **DataOverlapTripletVae.overlap_measure_differences(new_ds_posterior, aug_xs_targ, unreduced_loss_fn=self.recon_handler.compute_unreduced_loss, unreduced_kl_loss_fn=self.latents_handler.compute_unreduced_kl_loss),
-        }
 
     @staticmethod
-    def overlap_create_triplets(ds_posterior, xs_targ, cfg: cfg, unreduced_loss_fn):
+    def overlap_swap_triplet_idxs(x_targ, a_idxs, p_idxs, n_idxs, cfg: cfg, unreduced_loss_fn):
+        xs_targ = [x_targ[idxs] for idxs in (a_idxs, p_idxs, n_idxs)]
         # check the recon loss
         assert cfg.recon_loss == 'mse', 'only mse loss is supported'
         # CORE: order the latent variables for triplet
         swap_mask = DataOverlapTripletVae.overlap_swap_mask(xs_targ=xs_targ, unreduced_loss_fn=unreduced_loss_fn)
-        ds_posterior_NEW = DataOverlapTripletVae.overlap_get_swapped(ds_posterior=ds_posterior, swap_mask=swap_mask)
+        # swap all idxs
+        swapped_a_idxs = a_idxs
+        swapped_p_idxs = torch.where(swap_mask, n_idxs, p_idxs)
+        swapped_n_idxs = torch.where(swap_mask, p_idxs, n_idxs)
         # return values
-        return ds_posterior_NEW
+        return swapped_a_idxs, swapped_p_idxs, swapped_n_idxs
+
 
     @staticmethod
     def overlap_swap_mask(xs_targ: Sequence[torch.Tensor], unreduced_loss_fn: callable) -> torch.Tensor:
@@ -118,24 +118,6 @@ class DataOverlapTripletVae(AdaNegTripletVae):
         swap_mask = (a_p_losses > a_n_losses)
         # ++++++++++++++++++++++++++++++++++++++++++ #
         return swap_mask # (B,)
-
-    @staticmethod
-    def overlap_get_swapped(ds_posterior: Sequence[Normal], swap_mask: torch.Tensor) -> Sequence[Normal]:
-        # get variables
-        a_post_OLD, p_post_OLD, n_post_OLD = ds_posterior
-        # check variables
-        assert swap_mask.ndim == 1
-        assert a_post_OLD.mean.ndim == 2
-        assert len(swap_mask) == len(a_post_OLD.mean)
-        # repeat mask along latent dim
-        swap_mask = swap_mask[:, None].repeat(1, a_post_OLD.mean.shape[-1])  # (B,) -> (B, Z)
-        # swap if wrong!
-        p_z_loc   = torch.where(swap_mask, n_post_OLD.loc,   p_post_OLD.loc)
-        n_z_loc   = torch.where(swap_mask, p_post_OLD.loc,   n_post_OLD.loc)
-        p_z_scale = torch.where(swap_mask, n_post_OLD.scale, p_post_OLD.scale)
-        n_z_scale = torch.where(swap_mask, p_post_OLD.scale, n_post_OLD.scale)
-        # return new distributions
-        return a_post_OLD, Normal(loc=p_z_loc, scale=p_z_scale), Normal(loc=n_z_loc, scale=n_z_scale)
 
     def augment_triplet_targets(self, xs_targ):
         if self.cfg.overlap_augment_mode == 'none':
@@ -209,39 +191,6 @@ class DataOverlapTripletVae(AdaNegTripletVae):
             raise KeyError(f'invalid cfg.overlap_mine_triplet_mode=={repr(self.cfg.overlap_mine_triplet_mode)}')
         # -~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~- #
         return a_idxs, p_idxs, n_idxs
-
-    @staticmethod
-    def overlap_measure_differences(ds_posterior, xs_targ, unreduced_loss_fn, unreduced_kl_loss_fn):
-        d0_posterior, d1_posterior, d2_posterior = ds_posterior
-        x0_targ, x1_targ, x2_targ = xs_targ
-
-        # TESTS
-        # -~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~- #
-        # EFFECTIVELY SUMMED MSE WITH TERM: (u_1 - u_0)**2
-        kl_0_1 = unreduced_kl_loss_fn(d0_posterior, d1_posterior)
-        kl_0_2 = unreduced_kl_loss_fn(d0_posterior, d2_posterior)
-        mu_0_1 = (d0_posterior.mean - d1_posterior.mean) ** 2
-        mu_0_2 = (d0_posterior.mean - d2_posterior.mean) ** 2
-        # z differences
-        kl_mu_differences_all = torch.mean(((kl_0_1 < kl_0_2) ^ (mu_0_1 < mu_0_2)).to(torch.float32))
-        kl_mu_differences_ave = torch.mean(((kl_0_1.mean(dim=-1) < kl_0_2.mean(dim=-1)) ^ (mu_0_1.mean(dim=-1) < mu_0_2.mean(dim=-1))).to(torch.float32))
-        # obs differences
-        xs_0_1 = unreduced_loss_fn(x0_targ, x1_targ).mean(dim=(-3, -2, -1))
-        xs_0_2 = unreduced_loss_fn(x0_targ, x2_targ).mean(dim=(-3, -2, -1))
-        # get differences
-        kl_xs_differences_all = torch.mean(((kl_0_1 < kl_0_2) ^ (xs_0_1 < xs_0_2)[..., None]).to(torch.float32))
-        mu_xs_differences_all = torch.mean(((mu_0_1 < mu_0_2) ^ (xs_0_1 < xs_0_2)[..., None]).to(torch.float32))
-        kl_xs_differences_ave = torch.mean(((kl_0_1.mean(dim=-1) < kl_0_2.mean(dim=-1)) ^ (xs_0_1 < xs_0_2)).to(torch.float32))
-        mu_xs_differences_ave = torch.mean(((mu_0_1.mean(dim=-1) < mu_0_2.mean(dim=-1)) ^ (xs_0_1 < xs_0_2)).to(torch.float32))
-        # -~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~- #
-        return {
-            'overlap_kl_mu_diffs_all': kl_mu_differences_all,
-            'overlap_kl_mu_diffs_ave': kl_mu_differences_ave,
-            'overlap_kl_xs_diffs_all': kl_xs_differences_all,
-            'overlap_mu_xs_diffs_all': mu_xs_differences_all,
-            'overlap_kl_xs_diffs_ave': kl_xs_differences_ave,
-            'overlap_mu_xs_diffs_ave': mu_xs_differences_ave,
-        }
 
 
 # ========================================================================= #
