6037bcd6d81ada7f9986e31ad33aff7f1282318a
Nathan Michlo <NathanJMichlo@gmail.com>
2021-08-09 22:42:18 +0200

    global registry

---
+++ tests/test_registry.py
@@ -0,0 +1,61 @@
+#  ~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~
+#  MIT License
+#
+#  Copyright (c) 2021 Nathan Juraj Michlo
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in
+#  all copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#  SOFTWARE.
+#  ~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~
+
+
+from disent.registry import REGISTRY
+
+
+# ========================================================================= #
+# TESTS                                                                     #
+# ========================================================================= #
+
+
+def test_registry_loading():
+    COUNTS = {
+        'dataset': 9,
+        'sampler': 8,
+        'framework': 10,
+        'recon_loss': 8,
+        'latent_dist': 2,
+        'optimizer': 38,
+        'metric': 7,
+        'schedule': 5,
+        'model_encoder': 4,
+        'model_decoder': 4,
+    }
+    # load everything and check the counts
+    total = 0
+    for registry in REGISTRY:
+        count = 0
+        for name in REGISTRY[registry]:
+            loaded = REGISTRY[registry][name]
+            count += 1
+            total += 1
+        assert COUNTS[registry] == count
+    assert total == sum(COUNTS.values())
+
+
+# ========================================================================= #
+# END                                                                       #
+# ========================================================================= #
