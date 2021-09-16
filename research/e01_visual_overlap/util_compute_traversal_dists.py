63fb279b5443313ce3e02a437e696aef735df15f
Nathan Michlo <NathanJMichlo@gmail.com>
2021-09-16 13:16:28 +0200

    Much faster traversal distance computation with Ray

--- research/e01_visual_overlap/util_compute_traversal_dists.py
+++ research/e01_visual_overlap/util_compute_traversal_dists.py
@@ -21,10 +21,14 @@
 #  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #  SOFTWARE.
 #  ~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~
+import warnings
+from typing import Sequence
+
+import psutil
+import ray
+
 import logging
 import os
-from concurrent.futures import ThreadPoolExecutor
-from functools import partial
 from typing import Tuple
 
 import numpy as np
@@ -81,79 +85,105 @@ def print_dist_matrix_stats(gt_data: GroundTruthData):
 # ========================================================================= #
 
 
+def _iter_batch_ranges(total, batch_size):
+    assert total >= 0
+    assert batch_size > 0
+    for i in range(0, total, batch_size):
+        yield range(i, min(i + batch_size, total))
+
+
 def _check_gt_data(gt_data: GroundTruthData):
     obs = gt_data[0]
     # checks
     assert isinstance(obs, torch.Tensor)
     assert obs.dtype == torch.float32
 
-def _compute_dist(
-    idx: int,
+
+@ray.remote
+def _compute_dists(
+    idxs: Sequence[int],
     # thread data
-    f_states,
-    f_idx,
-    gt_data,
-    masked,
-    a_idxs,
-    b_idxs,
+    f_states: StateSpace,
+    f_idx: int,
+    gt_data: GroundTruthData,
+    masked: bool,
+    a_idxs: np.ndarray,
+    b_idxs: np.ndarray,
 ):
-    # translate traversal position to dataset position
-    base_pos = f_states.idx_to_pos(int(idx))
-    base_factors = np.insert(base_pos, f_idx, 0)
-    # load traversal: (f_size, H*W*C)
-    traversal = [gt_data[i].flatten().numpy() for i in gt_data.iter_traversal_indices(f_idx=f_idx, base_factors=base_factors)]
-    traversal = np.stack(traversal, axis=0)
-    # compute distances
-    if masked:
-        B, NUM = traversal.shape
-        # compute mask
-        mask = (traversal[0] != traversal[1])
-        for item in traversal[2:]:
-            mask |= (traversal[0] != item)
-        traversal = traversal[:, mask]
+    results = []
+    for idx in idxs:
+        # translate traversal position to dataset position
+        base_pos = f_states.idx_to_pos(int(idx))
+        base_factors = np.insert(base_pos, f_idx, 0)
+        # load traversal: (f_size, H*W*C)
+        traversal = [gt_data[i].flatten().numpy() for i in gt_data.iter_traversal_indices(f_idx=f_idx, base_factors=base_factors)]
+        traversal = np.stack(traversal, axis=0)
         # compute distances
-        dists = np.sum((traversal[a_idxs] - traversal[b_idxs]) ** 2, axis=1, dtype='float32') / NUM  # might need to be float64
-    else:
-        dists = np.mean((traversal[a_idxs] - traversal[b_idxs]) ** 2, axis=1, dtype='float32')
-    # return data
-    return base_pos, dists
+        if masked:
+            B, NUM = traversal.shape
+            # compute mask
+            mask = (traversal[0] != traversal[1])
+            for item in traversal[2:]:
+                mask |= (traversal[0] != item)
+            traversal = traversal[:, mask]
+            # compute distances
+            dists = np.sum((traversal[a_idxs] - traversal[b_idxs]) ** 2, axis=1, dtype='float32') / NUM  # might need to be float64
+        else:
+            dists = np.mean((traversal[a_idxs] - traversal[b_idxs]) ** 2, axis=1, dtype='float32')
+        # return data
+        results.append((base_pos, dists))
+    return results
 
 
+def get_as_completed(obj_ids):
+    # https://github.com/ray-project/ray/issues/5554
+    while obj_ids:
+        done, obj_ids = ray.wait(obj_ids)
+        yield ray.get(done[0])
+
+
+@torch.no_grad()
 def compute_factor_dist_matrices(
     gt_data: GroundTruthData,
     f_idx: int,
     masked: bool = True,
-    workers_num: int = min(os.cpu_count(), 16),
-    workers_chunksize: int = 1,
+    traversals_per_batch: int = 64,
 ):
+    if not ray.is_initialized():
+        warnings.warn(f'Ray has not yet been initialized, consider calling `ray.init(...)` and specifying the CPU requirements.')
     _check_gt_data(gt_data)
     # load data
     f_states = StateSpace(factor_sizes=np.delete(gt_data.factor_sizes, f_idx))
     a_idxs, b_idxs = H.pair_indices_combinations(gt_data.factor_sizes[f_idx])
     total = len(f_states)
-    # compute distances function, takes in an index of the traversal to compute the distance for!
-    # TODO: can this be improved with: ThreadPoolExecutor(..., initializer=???, initargs=???)
-    _compute_dist_fn = partial(
-        _compute_dist,
-        f_states=f_states,
-        f_idx=f_idx,
-        gt_data=gt_data,
-        masked=masked,
-        a_idxs=a_idxs,
-        b_idxs=b_idxs,
-    )
-    # store distances
+    # move to shared memory
+    ID_f_states = ray.put(f_states)
+    ID_gt_data = ray.put(gt_data)
+    ID_a_idxs = ray.put(a_idxs)
+    ID_b_idxs = ray.put(b_idxs)
+    # results
     f_dist_matrices = np.zeros(factor_dist_matrix_shape(gt_data=gt_data, f_idx=f_idx), dtype='float32')
+    # generate futures
+    futures = [
+        _compute_dists.remote(
+            idxs=sub_range,
+            f_idx=f_idx,
+            masked=masked,
+            f_states=ID_f_states,
+            gt_data=ID_gt_data,
+            a_idxs=ID_a_idxs,
+            b_idxs=ID_b_idxs,
+        )
+        for sub_range in _iter_batch_ranges(total, batch_size=traversals_per_batch)
+    ]
     # apply multithreading to compute traversal distances
-    # -- TODO: this is slow because there is no pre-fetching of data
-    log.debug(f'Creating thread pool with max_workers={repr(workers_num)} and chunksize={repr(workers_chunksize)}')
-    with ThreadPoolExecutor(max_workers=workers_num, thread_name_prefix=f'f{f_idx}') as executor:
-        with tqdm(total=total, desc=f'{gt_data.name}: {f_idx+1} of {gt_data.num_factors}') as p:
-            # compute distance matrices
-            for base_pos, dists in executor.map(_compute_dist_fn, range(total), chunksize=workers_chunksize):
+    with tqdm(total=total, desc=f'{gt_data.name}: {f_idx+1} of {gt_data.num_factors}') as p:
+        # compute distance matrices
+        for results in get_as_completed(futures):
+            for base_pos, dists in results:
                 f_dist_matrices[(*base_pos, a_idxs, b_idxs)] = dists
                 f_dist_matrices[(*base_pos, b_idxs, a_idxs)] = dists
-                p.update()
+            p.update(len(results))
     # return distances
     return f_dist_matrices
 
@@ -161,8 +191,7 @@ def compute_factor_dist_matrices(
 def compute_all_factor_dist_matrices(
     gt_data: GroundTruthData,
     masked: bool = True,
-    workers_num: int = min(os.cpu_count(), 16),
-    workers_chunksize: int = 1,
+    traversals_per_batch: int = 64,
 ):
     """
     ALGORITHM:
@@ -183,8 +212,7 @@ def compute_all_factor_dist_matrices(
             gt_data=gt_data,
             f_idx=f_idx,
             masked=masked,
-            workers_num=workers_num,
-            workers_chunksize=workers_chunksize,
+            traversals_per_batch=traversals_per_batch,
         )
         all_dist_matrices.append(f_dist_matrices)
     return all_dist_matrices
@@ -194,9 +222,7 @@ def compute_all_factor_dist_matrices(
 def cached_compute_all_factor_dist_matrices(
     dataset_name: str = 'smallnorb',
     masked: bool = False,
-    # comupute settings
-    workers_num: int = min(os.cpu_count(), 16),
-    workers_chunksize: int = 1,
+    traversals_per_batch: int = 64,
     # cache settings
     cache_dir: str = 'data/cache',
     force: bool = False,
@@ -213,7 +239,7 @@ def cached_compute_all_factor_dist_matrices(
         print(f'generating cached distances for: {dataset_name} to: {cache_path}')
         # generate & save
         with AtomicSaveFile(file=cache_path, overwrite=force) as path:
-            all_dist_matrices = compute_all_factor_dist_matrices(gt_data, masked=masked, workers_num=workers_num, workers_chunksize=workers_chunksize)
+            all_dist_matrices = compute_all_factor_dist_matrices(gt_data, masked=masked, traversals_per_batch=traversals_per_batch)
             np.savez(path, **{f_name: f_dists for f_name, f_dists in zip(gt_data.factor_names, all_dist_matrices)})
     # load data
     print(f'loading cached distances for: {dataset_name} from: {cache_path}')
@@ -226,15 +252,16 @@ def cached_compute_all_factor_dist_matrices(
 # ========================================================================= #
 
 
-@torch.no_grad()
-def main():
-    for name in ['smallnorb', 'cars3d', 'shapes3d', 'dsprites', 'xysquares']:
+def generate_common_cache():
+    for name in ['cars3d', 'smallnorb', 'shapes3d', 'dsprites', 'xysquares']:
         # get the dataset and delete the transform
         gt_data = H.make_data(name, transform_mode='float32')
         print_dist_matrix_stats(gt_data)
         f_dist_matrices = cached_compute_all_factor_dist_matrices(
             dataset_name=name,
             force=True,
+            masked=True,
+            traversals_per_batch=32,
         )
         # plot distance matrices
         H.plt_subplots_imshow(
@@ -245,5 +272,14 @@ def main():
         plt.show()
 
 
+def _test_masked_equals_unmasked():
+    for name in ['cars3d', 'smallnorb', 'shapes3d', 'dsprites', 'xysquares']:
+        dists_a = compute_all_factor_dist_matrices(gt_data=H.make_data(name, transform_mode='float32'), masked=True, traversals_per_batch=32)
+        dists_b = compute_all_factor_dist_matrices(gt_data=H.make_data(name, transform_mode='float32'), masked=False, traversals_per_batch=32)
+        for a, b in zip(dists_a, dists_b):
+            assert np.allclose(a, b)
+
+
 if __name__ == '__main__':
-    main()
+    ray.init(num_cpus=min(os.cpu_count(), 32))
+    generate_common_cache()
