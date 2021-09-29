97432721a9b74a0a349d7f91b29dd58838ab4491
Nathan Michlo <NathanJMichlo@gmail.com>
2021-09-29 13:52:57 +0200

    update run experiment file

--- research/e06_adversarial_data/run_04_train_masked_data.sh
+++ research/e06_adversarial_data/run_04_train_masked_data.sh
@@ -17,12 +17,11 @@ source "$(dirname "$(dirname "$(realpath -s "$0")")")/helper.sh"
 
 clog_cudaless_nodes "$PARTITION" 86400 "C-disent" # 24 hours
 
-# 2 * (2*8) = 32
+# 2 * (2*4*2) = 32
 submit_sweep \
     +DUMMY.repeat=1,2 \
-    +EXTRA.tags='cluster-run-1' \
-    run_length=short \
-    dataset=cars3d,dsprites,X--mask-adv-cars3d,X--mask-adv-smallnorb,X--mask-dthr-cars3d,X--mask-dthr-smallnorb,X--mask-ran-cars3d,X--mask-ran-smallnorb \
-    model.z_size=9 \
-    framework=betavae,adavae_os \
-    specializations.dataset_sampler='random_${framework.data_sample_mode}'
+    +EXTRA.tags='sweep_1' \
+    run_length=tiny \
+    -m framework=betavae,adavae_os \
+    dataset=X--mask-adv-shapes3d,X--mask-ran-shapes3d,X--mask-dthr-shapes3d,shapes3d \
+    model.z_size=9,25
