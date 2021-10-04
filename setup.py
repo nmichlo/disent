67178514f23485c4d135dffeacd0200b33775180
Nathan Michlo <NathanJMichlo@gmail.com>
2021-10-04 16:21:28 +0200

    py39 version for pypi

--- setup.py
+++ setup.py
@@ -63,6 +63,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Intended Audience :: Science/Research",
     ],
 )
