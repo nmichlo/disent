277f75f42fa73130a614cc59ab41874034b7c462
Nathan Michlo <NathanJMichlo@gmail.com>
2021-10-21 02:00:53 +0200

    experiment fix

--- research/e06_adversarial_data/run_04_train_masked_data_dist_pairs.sh
+++ research/e06_adversarial_data/run_04_train_masked_data_dist_pairs.sh
@@ -19,19 +19,21 @@ source "$(dirname "$(dirname "$(realpath -s "$0")")")/helper.sh"
 clog_cudaless_nodes "$PARTITION" 86400 "C-disent" # 24 hours
 
 # (3*2*3*12 = 72) = 216
+# -- accidentally ran uploaded version with `run_callbacks=vis` instead
 submit_sweep \
     +DUMMY.repeat=1 \
     +EXTRA.tags='sweep_dist_pairs_usage_ratio' \
     \
+    run_callbacks=vis_slow \
     run_length=short \
     metrics=all_fast_final \
     \
     framework.beta=0.0316,0.01,0.1 \
     framework=betavae,adavae_os \
-    model.z_size=25 \
+    model.z_size=16 \
     framework.optional.usage_ratio=0.5,0.2,0.05 \
     \
     dataset=X--mask-adv-r-dsprites,X--mask-ran-dsprites,dsprites,X--mask-adv-r-shapes3d,X--mask-ran-shapes3d,shapes3d,X--mask-adv-r-smallnorb,X--mask-ran-smallnorb,smallnorb,X--mask-adv-r-cars3d,X--mask-ran-cars3d,cars3d \
     sampling=random \
     \
-    hydra.launcher.exclude='"mscluster93,mscluster94,mscluster97"'  # we don't want to sweep over these
+    hydra.launcher.exclude='"mscluster93,mscluster94,mscluster97,mscluster99"'  # we don't want to sweep over these
