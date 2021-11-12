15868cfbb7119e8664198b1c773da7479700e91f
Nathan Michlo <NathanJMichlo@gmail.com>
2021-11-12 18:19:31 +0200

    update experiment

--- research/e01_incr_overlap/submit_incr_overlap.sh
+++ research/e01_incr_overlap/submit_incr_overlap.sh
@@ -16,6 +16,9 @@ source "$(dirname "$(dirname "$(realpath -s "$0")")")/helper.sh"
 # Experiment                                                                #
 # ========================================================================= #
 
+clog_cudaless_nodes "$PARTITION" 86400 "C-disent" # 24 hours
+
+
 # background launch various xysquares
 # -- original experiment also had dfcvae
 # 5 * (2*2*8 = 32) = 160
@@ -31,23 +34,23 @@ submit_sweep \
     settings.model.z_size=9 \
     \
     sampling=default__bb \
-    dataset=xysquares \
+    dataset=X--xysquares_rgb \
     dataset.data.grid_spacing=8,7,6,5,4,3,2,1
 
 
 # background launch traditional datasets
 # -- original experiment also had dfcvae
 # 5 * (2*2*4 = 16) = 80
-submit_sweep \
-    +DUMMY.repeat=1,2,3,4,5 \
-    +EXTRA.tags='sweep_other' \
-    \
-    run_length=medium \
-    metrics=all \
-    \
-    settings.framework.beta=0.01,0.0316 \
-    framework=betavae,adavae_os \
-    settings.model.z_size=9 \
-    \
-    sampling=default__bb \
-    dataset=cars3d,shapes3d,dsprites,smallnorb
+#submit_sweep \
+#    +DUMMY.repeat=1,2,3,4,5 \
+#    +EXTRA.tags='sweep_other' \
+#    \
+#    run_length=medium \
+#    metrics=all \
+#    \
+#    settings.framework.beta=0.01,0.0316 \
+#    framework=betavae,adavae_os \
+#    settings.model.z_size=9 \
+#    \
+#    sampling=default__bb \
+#    dataset=cars3d,shapes3d,dsprites,smallnorb
