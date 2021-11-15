cea43fa749e602da21240b83eff2561a705f1804
Nathan Michlo <NathanJMichlo@gmail.com>
2021-11-15 14:18:37 +0200

    fix experiments + more plots

--- research/e01_incr_overlap/submit_incr_overlap.sh
+++ research/e01_incr_overlap/submit_incr_overlap.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
+
+# OVERVIEW:
+# - this experiment is designed to check how increasing overlap (reducing
+#   the spacing between square positions on XYSquares) affects learning.
+
+
+# OUTCOMES:
+# - increasing overlap improves disentanglement & ability for the
+#   neural network to learn values.
+# - decreasing overlap worsens disentanglement, but it also becomes
+#    very hard for the neural net to learn specific values needed. The
+#    average image does not correspond well to individual samples.
+#    Disentanglement performance is also a result of this fact, as
+#    the network can't always learn the dataset effectively.
+
+
 # ========================================================================= #
 # Settings                                                                  #
 # ========================================================================= #
+
 
 export USERNAME="n_michlo"
 export PROJECT="CVPR-01__incr_overlap"
@@ -12,9 +29,11 @@ export PARALLELISM=28
 # source the helper file
 source "$(dirname "$(dirname "$(realpath -s "$0")")")/helper.sh"
 
+
 # ========================================================================= #
 # Experiment                                                                #
 # ========================================================================= #
+
 
 clog_cudaless_nodes "$PARTITION" 86400 "C-disent" # 24 hours
 
@@ -22,9 +41,9 @@ clog_cudaless_nodes "$PARTITION" 86400 "C-disent" # 24 hours
 # background launch various xysquares
 # -- original experiment also had dfcvae
 # -- beta is too high for adavae
-# 3 * (2*2*8 = 32) = 96
+# 5 * (2*2*8 = 32) = 160
 submit_sweep \
-    +DUMMY.repeat=1,2,3 \
+    +DUMMY.repeat=1,2,3,4,5 \
     +EXTRA.tags='sweep_xy_squares_overlap' \
     hydra.job.name="incr_ovlp" \
     \
@@ -41,25 +60,48 @@ submit_sweep \
 
 
 # background launch various xysquares
-# - this time we try delay beta, so that it can learn properly...
-# 3 * (2*2*8 = 32) = 96
+# -- original experiment also had dfcvae
+# -- beta is too high for adavae
+# 5 * (2*8 = 16) = 80
 submit_sweep \
-    +DUMMY.repeat=1,2,3 \
-    +EXTRA.tags='sweep_xy_squares_overlap_delay' \
-    hydra.job.name="schd_incr_ovlp" \
-    \
-    schedule=beta_delay_long \
+    +DUMMY.repeat=1,2,3,4,5 \
+    +EXTRA.tags='sweep_xy_squares_overlap_small_beta' \
+    hydra.job.name="sb_incr_ovlp" \
     \
     run_length=medium \
     metrics=all \
     \
-    settings.framework.beta=0.001 \
-    framework=betavae,adavae_os \
-    settings.model.z_size=9,25 \
+    settings.framework.beta=0.0001,0.00001 \
+    framework=adavae_os \
+    settings.model.z_size=9 \
     \
     sampling=default__bb \
     dataset=X--xysquares_rgb \
     dataset.data.grid_spacing=8,7,6,5,4,3,2,1
+
+
+# background launch various xysquares
+# - this time we try delay beta, so that it can learn properly...
+# - NOTE: this doesn't actually work, the VAE loss often becomes
+#         NAN because the values are too small.
+# 3 * (2*2*8 = 32) = 96
+# submit_sweep \
+#     +DUMMY.repeat=1,2,3 \
+#     +EXTRA.tags='sweep_xy_squares_overlap_delay' \
+#     hydra.job.name="schd_incr_ovlp" \
+#     \
+#     schedule=beta_delay_long \
+#     \
+#     run_length=medium \
+#     metrics=all \
+#     \
+#     settings.framework.beta=0.001 \
+#     framework=betavae,adavae_os \
+#     settings.model.z_size=9,25 \
+#     \
+#     sampling=default__bb \
+#     dataset=X--xysquares_rgb \
+#     dataset.data.grid_spacing=8,7,6,5,4,3,2,1
 
 
 # background launch traditional datasets
@@ -78,3 +120,8 @@ submit_sweep \
 #    \
 #    sampling=default__bb \
 #    dataset=cars3d,shapes3d,dsprites,smallnorb
+
+
+# ========================================================================= #
+# DONE                                                                      #
+# ========================================================================= #
--- research/e09_vae_overlap_loss/submit_overlap_loss.sh
+++ research/e09_vae_overlap_loss/submit_overlap_loss.sh
@@ -19,10 +19,33 @@ source "$(dirname "$(dirname "$(realpath -s "$0")")")/helper.sh"
 clog_cudaless_nodes "$PARTITION" 86400 "C-disent" # 24 hours
 
 # 1 * (5 * 2*4*2) = 80
+#submit_sweep \
+#    +DUMMY.repeat=1 \
+#    +EXTRA.tags='sweep_overlap_boxblur' \
+#    hydra.job.name="ovlp_loss" \
+#    \
+#    +VAR.recon_loss_weight=1.0 \
+#    +VAR.kernel_loss_weight=3969.0 \
+#    +VAR.kernel_radius=31 \
+#    \
+#    run_length=medium \
+#    metrics=all \
+#    \
+#    dataset=X--xysquares,dsprites,shapes3d,smallnorb,cars3d \
+#    \
+#    framework=betavae,adavae_os \
+#    settings.framework.beta=0.0316,0.316,0.1,0.01 \
+#    settings.model.z_size=25,9 \
+#    settings.framework.recon_loss='mse_box_r${VAR.kernel_radius}_l${VAR.recon_loss_weight}_k${VAR.kernel_loss_weight}' \
+#    \
+#    sampling=default__bb
+
+
+# 5 * (2*2*2 = 8) = 40
 submit_sweep \
-    +DUMMY.repeat=1 \
-    +EXTRA.tags='sweep_overlap_boxblur' \
-    hydra.job.name="ovlp_loss" \
+    +DUMMY.repeat=1,2,3,4,5 \
+    +EXTRA.tags='sweep_overlap_boxblur_specific' \
+    hydra.job.name="s_ovlp_loss" \
     \
     +VAR.recon_loss_weight=1.0 \
     +VAR.kernel_loss_weight=3969.0 \
@@ -31,11 +54,30 @@ submit_sweep \
     run_length=medium \
     metrics=all \
     \
-    dataset=X--xysquares,dsprites,shapes3d,smallnorb,cars3d \
+    dataset=X--xysquares \
     \
     framework=betavae,adavae_os \
-    settings.framework.beta=0.0316,0.316,0.1,0.01 \
-    settings.model.z_size=25,9 \
-    settings.framework.recon_loss='mse_box_r${VAR.kernel_radius}_l${VAR.recon_loss_weight}_k${VAR.kernel_loss_weight}' \
+    settings.framework.beta=0.0316,0.0001 \
+    settings.model.z_size=25 \
+    settings.framework.recon_loss=mse,'mse_box_r${VAR.kernel_radius}_l${VAR.recon_loss_weight}_k${VAR.kernel_loss_weight}' \
     \
     sampling=default__bb
+
+
+# HPARAM SWEEP -- TODO: update
+# 1 * (2 * 8 * 2 * 2) = 160
+#submit_sweep \
+#    +DUMMY.repeat=1 \
+#    +EXTRA.tags='sweep_beta' \
+#    hydra.job.name="vae_hparams" \
+#    \
+#    run_length=long \
+#    metrics=all \
+#    \
+#    settings.framework.beta=0.000316,0.001,0.00316,0.01,0.0316,0.1,0.316,1.0 \
+#    framework=betavae,adavae_os \
+#    schedule=none \
+#    settings.model.z_size=9,25 \
+#    \
+#    dataset=X--xysquares \
+#    sampling=default__bb
