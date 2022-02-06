1817000a0e45af3b2fb395fad4ddc32b5f44b75f
Nathan Michlo <NathanJMichlo@gmail.com>
2022-02-06 12:59:58 +0200

    version bump v0.3.4

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setuptools.setup(
     author="Nathan Juraj Michlo",
     author_email="NathanJMichlo@gmail.com",
 
-    version="0.3.3",
+    version="0.3.4",
     python_requires=">=3.8",  # we make use of standard library features only in 3.8
     packages=setuptools.find_packages(),
 
