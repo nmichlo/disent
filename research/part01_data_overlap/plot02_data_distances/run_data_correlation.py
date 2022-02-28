029bb7be65c6a9fa3f388dde197491b0fc423551
Nathan Michlo <NathanJMichlo@gmail.com>
2022-02-28 02:37:27 +0200

    update compute dataset correlation

--- research/part01_data_overlap/plot02_data_distances/run_data_correlation.py
+++ research/part01_data_overlap/plot02_data_distances/run_data_correlation.py
@@ -23,35 +23,25 @@
 #  ~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~
 
 
-import os
-from collections import defaultdict
-from typing import Dict
 from typing import Optional
 from typing import Tuple
 from typing import Union
 
-import seaborn as sns
 import numpy as np
-import pandas as pd
 import torch
 from scipy.stats import spearmanr, pearsonr
 
 from disent.nn.loss.reduction import batch_loss_reduction
-from matplotlib import pyplot as plt
-from matplotlib.ticker import MultipleLocator
 from tqdm import tqdm
 import torch.nn.functional as F
 
-import research.code.util as H
 from disent.dataset import DisentDataset
 from disent.dataset.data import Cars3d64Data
 from disent.dataset.data import DSpritesData
-from disent.dataset.data import GroundTruthData
 from disent.dataset.data import Shapes3dData
 from disent.dataset.data import SmallNorb64Data
 from research.code.dataset.data import XYSquaresData
 from disent.dataset.transform import ToImgTensorF32
-from disent.util import to_numpy
 from disent.util.function import wrapped_partial
 
 
@@ -63,22 +53,20 @@ from disent.util.function import wrapped_partial
 
 # NOTE: this should match _factored_components._dists_compute_scores!
 #       -- this is taken directly from there!
-def _compute_rcorr_ground_data(num_triplets: int, xs_traversal: torch.Tensor, factors: torch.Tensor) -> Tuple[float, float]:
+def _compute_rcorr_ground_data(num_samples: int, xs_traversal: torch.Tensor, factors: torch.Tensor) -> Tuple[float, float]:
     # checks
     assert len(factors) == len(xs_traversal)
     assert factors.device == xs_traversal.device
     # generate random triplets
     # - {p, n} indices do not need to be sorted like triplets, these can be random.
     #   This metric is symmetric for swapped p & n values.
-    idxs_a, idxs_p, idxs_n = torch.randint(0, len(xs_traversal), size=(3, num_triplets), device=xs_traversal.device)
+    idxs_a, idxs_b = torch.randint(0, len(xs_traversal), size=(2, num_samples), device=xs_traversal.device)
     # compute distances -- shape: (num,)
-    ap_ground_dists = torch.norm(factors[idxs_a, :] - factors[idxs_p, :], p=1, dim=-1)
-    an_ground_dists = torch.norm(factors[idxs_a, :] - factors[idxs_n, :], p=1, dim=-1)
-    ap_data_dists = batch_loss_reduction(F.mse_loss(xs_traversal[idxs_a, ...], xs_traversal[idxs_p, ...], reduction='none'), reduction_dtype=torch.float32, reduction='mean')
-    an_data_dists = batch_loss_reduction(F.mse_loss(xs_traversal[idxs_a, ...], xs_traversal[idxs_n, ...], reduction='none'), reduction_dtype=torch.float32, reduction='mean')
+    ground_dists = torch.norm(factors[idxs_a, :] - factors[idxs_b, :], p=1, dim=-1)
+    data_dists = batch_loss_reduction(F.mse_loss(xs_traversal[idxs_a, ...], xs_traversal[idxs_b, ...], reduction='none'), reduction_dtype=torch.float32, reduction='mean')
     # concatenate values -- shape: (2 * num,)
-    ground_dists    = torch.cat([ap_ground_dists,    an_ground_dists],    dim=0).numpy()
-    data_dists      = torch.cat([ap_data_dists,      an_data_dists],      dim=0).numpy()
+    ground_dists = ground_dists.numpy()
+    data_dists = data_dists.numpy()
     # compute rcorr scores -- shape: ()
     # - compute the pearson rank correlation coefficient over the concatenated distances
     linear_corr, _ = pearsonr(ground_dists, data_dists)
@@ -87,7 +75,7 @@ def _compute_rcorr_ground_data(num_triplets: int, xs_traversal: torch.Tensor, fa
     return linear_corr, rank_corr
 
 
-def _compute_mean_rcorr_ground_data(dataset: DisentDataset, f_idx: Optional[Union[str, int]], num_triplets: int, repeats: int, progress: bool = True, random_batch_size: Optional[int] = 64):
+def _compute_mean_rcorr_ground_data(dataset: DisentDataset, f_idx: Optional[Union[str, int]], num_samples: int, repeats: int, progress: bool = True, random_batch_size: Optional[int] = 64):
     # normalise everything!
     if f_idx in ('random', None):
         f_idx = None
@@ -113,7 +101,7 @@ def _compute_mean_rcorr_ground_data(dataset: DisentDataset, f_idx: Optional[Unio
         xs = dataset.dataset_batch_from_factors(factors, 'input').cpu()
         factors = torch.from_numpy(factors).to(torch.float32).cpu()
         # [COMPUTE SAME RATIO & CORRELATION]
-        linear_corr, rank_corr = _compute_rcorr_ground_data(num_triplets, xs_traversal=xs, factors=factors)
+        linear_corr, rank_corr = _compute_rcorr_ground_data(num_samples, xs_traversal=xs, factors=factors)
         # [UPDATE SCORES]
         correlations_linear.append(linear_corr)
         correlations_rank.append(rank_corr)
@@ -128,38 +116,46 @@ def _compute_mean_rcorr_ground_data(dataset: DisentDataset, f_idx: Optional[Unio
 
 if __name__ == '__main__':
 
-    gt_data_classes = {
-      # 'XYObject':  wrapped_partial(XYObjectData),
-      # 'XYBlocks':  wrapped_partial(XYBlocksData),
-      #   'XYSquares': wrapped_partial(XYSquaresData),
-        'DSprites':  wrapped_partial(DSpritesData),
-        'Shapes3d':  wrapped_partial(Shapes3dData),
-        'Cars3d':    wrapped_partial(Cars3d64Data),
-        'SmallNorb': wrapped_partial(SmallNorb64Data),
-      # 'Mpi3d':     wrapped_partial(Mpi3dData),
+    def main():
+        gt_data_classes = {
+          # 'XYObject':  wrapped_partial(XYObjectData),
+          # 'XYBlocks':  wrapped_partial(XYBlocksData),
+          #   'XYSquares': wrapped_partial(XYSquaresData),
+            'DSprites':  wrapped_partial(DSpritesData),
+            'Shapes3d':  wrapped_partial(Shapes3dData),
+            'Cars3d':    wrapped_partial(Cars3d64Data),
+            'SmallNorb': wrapped_partial(SmallNorb64Data),
+          # 'Mpi3d':     wrapped_partial(Mpi3dData),
 
-        'XYSquares-1-8': wrapped_partial(XYSquaresData, square_size=8, grid_spacing=1, grid_size=8, no_warnings=True),
-        'XYSquares-2-8': wrapped_partial(XYSquaresData, square_size=8, grid_spacing=2, grid_size=8, no_warnings=True),
-        'XYSquares-3-8': wrapped_partial(XYSquaresData, square_size=8, grid_spacing=3, grid_size=8, no_warnings=True),
-        'XYSquares-4-8': wrapped_partial(XYSquaresData, square_size=8, grid_spacing=4, grid_size=8, no_warnings=True),
-        'XYSquares-5-8': wrapped_partial(XYSquaresData, square_size=8, grid_spacing=5, grid_size=8, no_warnings=True),
-        'XYSquares-6-8': wrapped_partial(XYSquaresData, square_size=8, grid_spacing=6, grid_size=8, no_warnings=True),
-        'XYSquares-7-8': wrapped_partial(XYSquaresData, square_size=8, grid_spacing=7, grid_size=8, no_warnings=True),
-        'XYSquares-8-8': wrapped_partial(XYSquaresData, square_size=8, grid_spacing=8, grid_size=8, no_warnings=True),
-    }
+            'XYSquares-1-8': wrapped_partial(XYSquaresData, square_size=8, grid_spacing=1, grid_size=8, no_warnings=True),
+            'XYSquares-2-8': wrapped_partial(XYSquaresData, square_size=8, grid_spacing=2, grid_size=8, no_warnings=True),
+            'XYSquares-3-8': wrapped_partial(XYSquaresData, square_size=8, grid_spacing=3, grid_size=8, no_warnings=True),
+            'XYSquares-4-8': wrapped_partial(XYSquaresData, square_size=8, grid_spacing=4, grid_size=8, no_warnings=True),
+            'XYSquares-5-8': wrapped_partial(XYSquaresData, square_size=8, grid_spacing=5, grid_size=8, no_warnings=True),
+            'XYSquares-6-8': wrapped_partial(XYSquaresData, square_size=8, grid_spacing=6, grid_size=8, no_warnings=True),
+            'XYSquares-7-8': wrapped_partial(XYSquaresData, square_size=8, grid_spacing=7, grid_size=8, no_warnings=True),
+            'XYSquares-8-8': wrapped_partial(XYSquaresData, square_size=8, grid_spacing=8, grid_size=8, no_warnings=True),
+        }
 
-    num_triplets = 256
-    repeats = 2048
-    progress = False
+        num_samples = 64
+        repeats = 32768
+        progress = True
 
-    for name, data_cls in  gt_data_classes.items():
-        dataset = DisentDataset(data_cls(), transform=ToImgTensorF32(size=64))
-        factor_names = (*dataset.gt_data.factor_names, 'random')
-        # compute over each factor name
-        for f_name in [*dataset.gt_data.factor_names, 'random']:
-            linear_corr, rank_corr = _compute_mean_rcorr_ground_data(dataset, f_idx=f_name, num_triplets=num_triplets, repeats=repeats, progress=progress)
-            print(f'[{name}] f_idx={f_name:{max(len(s) for s in factor_names)}s} linear_corr={linear_corr:7.5f}, rank_corr={rank_corr:7.5f}')
-        print()
+        for name, data_cls in  gt_data_classes.items():
+            dataset = DisentDataset(data_cls(), transform=ToImgTensorF32(size=64))
+            factor_names = (*dataset.gt_data.factor_names, 'random')
+            # compute over each factor name
+            for f_name in [*dataset.gt_data.factor_names, 'random']:
+                f_size = dataset.gt_data.factor_sizes[dataset.gt_data.normalise_factor_idx(f_name)] if f_name != 'random' else len(dataset)
+                try:
+                    linear_corr, rank_corr = _compute_mean_rcorr_ground_data(dataset, f_idx=f_name, num_samples=num_samples, repeats=repeats, progress=progress)
+                    print(f'[{name}] f_idx={f_name:{max(len(s) for s in factor_names)}s} f_size={f_size} linear_corr={linear_corr:9.7f}, rank_corr={rank_corr:9.7f}')
+                except Exception as e:
+                    print(f'[{name}] f_idx={f_name:{max(len(s) for s in factor_names)}s} f_size={f_size} SKIPPED!')
+                    raise e
+            print()
+
+    main()
 
 
 # ========================================================================= #
