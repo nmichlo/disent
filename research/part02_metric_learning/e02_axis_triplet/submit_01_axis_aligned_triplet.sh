85d6a2d64a63659a38fe85d936419e0981e86a46
Nathan Michlo <NathanJMichlo@gmail.com>
2022-03-04 00:31:47 +0200

    fix axis align experiments

--- research/part02_metric_learning/e02_axis_triplet/submit_01_axis_aligned_triplet.sh
+++ research/part02_metric_learning/e02_axis_triplet/submit_01_axis_aligned_triplet.sh
@@ -15,7 +15,7 @@
 export USERNAME="n_michlo"
 export PROJECT="MSC-p02e02_axis-aligned-triplet"
 export PARTITION="stampede"
-export PARALLELISM=32
+export PARALLELISM=24
 
 # source the helper file
 source "$(dirname "$(dirname "$(dirname "$(realpath -s "$0")")")")/scripts/helper.sh"
@@ -24,32 +24,41 @@ source "$(dirname "$(dirname "$(dirname "$(realpath -s "$0")")")")/scripts/helpe
 # Experiment                                                                #
 # ========================================================================= #
 
-clog_cudaless_nodes "$PARTITION" 86400 "C-disent" # 24 hours
-
-# TODO: design experiment
+clog_cudaless_nodes "$PARTITION" 172800 "C-disent" # 48 hours
 
 # SWEEP FOR GOOD TVAE PARAMS
-#   1 * (5*9*3*1) = 135
-#submit_sweep \
-#    +DUMMY.repeat=1 \
-#    +EXTRA.tags='sweep_tvae_params_basic' \
-#    hydra.job.name="tvae_params" \
-#    \
-#    run_length=medium \
-#    metrics=all \
-#    \
-#    settings.framework.beta=0.01 \
-#    framework=tvae \
-#    settings.model.z_size=25 \
-#    \
-#    framework.cfg.triplet_scale=1.0 \
-#    framework.cfg.triplet_p=1 \
-#    framework.cfg.detach_decoder=FALSE \
-#    framework.cfg.triplet_loss=triplet_soft \
-#    \
-#    schedule=adavae_up_all,adavae_up_all_full,adavae_up_ratio,adavae_up_ratio_full,adavae_up_thresh \
-#    framework.cfg.adat_triplet_loss=triplet,triplet_soft_ave_neg,triplet_soft_ave_p_n,triplet_soft_ave_all,triplet_hard_ave,triplet_hard_neg_ave,triplet_hard_neg_ave_pull,triplet_hard_ave_all,triplet_hard_neg_ave_scaled \
-#    \
-#    dataset=smallnorb,shapes3d,X--xysquares \
-#    sampling=gt_dist__manhat
-#
+#   1 * (3*2*5*2*5) = 135
+submit_sweep \
+    +DUMMY.repeat=1 \
+    +EXTRA.tags='sweep_adanegtvae_params_basic' \
+    hydra.job.name="adanegtvae_params" \
+    \
+    run_length=medium \
+    metrics=all \
+    \
+    settings.framework.beta=0.01 \
+    settings.model.z_size=25 \
+    settings.optimizer.lr=1e-3,3e-4,1e-4 \
+    \
+    framework.cfg.detach_decoder=FALSE \
+    \
+    sampling=gt_dist__manhat,gt_dist__manhat_scaled \
+    schedule=adavae_up_all,adavae_up_all_full,adavae_up_ratio,adavae_up_ratio_full,adavae_up_thresh \
+    \
+    framework=X--adanegtvae \
+    framework.cfg.ada_thresh_mode=symmetric_kl,dist \
+    \
+    framework.cfg.triplet_margin_max=10.0 \
+    framework.cfg.triplet_scale=1.0 \
+    framework.cfg.triplet_p=1 \
+    framework.cfg.triplet_loss=triplet_soft \
+    \
+    dataset=cars3d,smallnorb,shapes3d,dsprites,X--xysquares
+
+
+# CHECK ADAVAE modes:
+# framework.cfg.adat_triplet_loss=triplet,triplet_soft_ave_neg,triplet_soft_ave_p_n,triplet_soft_ave_all,triplet_hard_ave,triplet_hard_neg_ave,triplet_hard_neg_ave_pull,triplet_hard_ave_all,triplet_hard_neg_ave_scaled \
+
+# TODO:
+# - try run length long
+# - try run detach
