9cf64ef04b071673ad706a8b69354d9763613065
Nathan Michlo <NathanJMichlo@gmail.com>
2022-03-10 13:43:43 +0200

    ability to launch jobs with batch files without hydra

--- research/scripts/helper.sh
+++ research/scripts/helper.sh
@@ -15,21 +15,22 @@
 # 3. exports a helper function that runs the script in the background, with
 #    the correct python path and settings
 
-if [ -z "$PROJECT" ]; then echo "PROJECT is not set"; exit 1; fi
-if [ -z "$PARTITION" ]; then echo "PARTITION is not set"; exit 1; fi
+if [ -z "$PROJECT" ];     then echo "PROJECT is not set";     exit 1; fi
+if [ -z "$PARTITION" ];   then echo "PARTITION is not set";   exit 1; fi
 if [ -z "$PARALLELISM" ]; then echo "PARALLELISM is not set"; exit 1; fi
-if [ -z "$USERNAME" ]; then echo "USERNAME is not set"; exit 1; fi
+if [ -z "$USERNAME" ];    then echo "USERNAME is not set";    exit 1; fi
 if [ -z "$PY_RUN_FILE" ]; then PY_RUN_FILE='experiment/run.py'; fi
-
-export PY_RUN_FILE
 
 # ========================================================================= #
 # Helper                                                                    #
 # ========================================================================= #
 
-# get the root directory
-SCRIPT_DIR=$(dirname "$(realpath -s "$0")")
-ROOT_DIR="$(realpath -s "$SCRIPT_DIR/../../..")"
+# get the calling script dir
+#SCRIPT_DIR=$(dirname "$(realpath -s "$0")")
+# get the current script dir
+SCRIPT_DIR="$(dirname -- "${BASH_SOURCE[0]}")"
+# get the root directory for `disent`
+ROOT_DIR="$(realpath -s "$SCRIPT_DIR/../..")"
 
 # cd into the root, exit on failure
 cd "$ROOT_DIR" || exit 1
@@ -37,6 +38,7 @@ echo "working directory is: $(pwd)"
 
 PY_RUN_FILE="$(realpath "$PY_RUN_FILE")"
 echo "main script is: $PY_RUN_FILE"
+export PY_RUN_FILE
 
 # hydra search path and plugins todo: make this configurable?
 _SEARCH_PATH="${ROOT_DIR}/research/config"
@@ -64,6 +66,7 @@ function local_run() {
         "$@"
 }
 
+# NOTE: this should correspond to the code in `sbatch_job.sh`
 function local_sweep() {
     echo "RUNNING SWEEP:" "$@"
     PYTHONPATH="$ROOT_DIR" DISENT_CONFIGS_PREPEND="$_SEARCH_PATH" python3 "$PY_RUN_FILE" \
@@ -74,18 +77,52 @@ function local_sweep() {
         "$@"
 }
 
-function gen_permutations() {
-    _base="PYTHONPATH=\"$ROOT_DIR\" DISENT_CONFIGS_PREPEND=\"$_SEARCH_PATH\" python3 \"$PY_RUN_FILE\""
+function gen_sbatch_args_file() {
+    # make sure we have a unique filename specified to store the args
+    if [ -z "$ARGS_START_NUM" ]; then ARGS_START_NUM=1 ; fi
+    if [ -z "$ARGS_FILE" ]; then ARGS_FILE="$(realpath "array_$PROJECT.txt")" ; fi
+    # make sure the args file is absolute
+    case "$ARGS_FILE" in
+      /*) true ;;
+      *) echo "ARGS_FILE is not absolute, got: $ARGS_FILE" ; exit 1 ;;
+    esac
+    # generate everything
+    echo "[GENERATING SWEEP]:" "$@"
+    echo
     # generate a single command for all the diferent permutations
-    PYTHONPATH="$ROOT_DIR" python3 "$_SCRIPTS_PATH/permutations.py" \
+    _args="$(PYTHONPATH="$ROOT_DIR" python3 "$_SCRIPTS_PATH/permutations.py" \
         --line-numbers \
+        --line-number-format="+EXTRA.sweep_num={}" \
+        --line-number-start="$ARGS_START_NUM" \
         --no-color \
-        --base="$_base" \
         --overrides \
-        run_launcher=local \
-        settings.job.project="$PROJECT" \
-        settings.job.user="$USERNAME" \
-        "$@"
+        "$@")"
+    # save to file
+    if [ -z "$APPEND_ARGS" ]; then
+      echo "$_args" > "$ARGS_FILE"
+      echo "[SAVED ARGS]: $ARGS_FILE"
+    else
+      echo "$_args" >> "$ARGS_FILE"
+      echo "[ADDED ARGS]: $ARGS_FILE"
+    fi
+    echo
+}
+
+function submit_sbatch_args_file() {
+    # make sure we have a unique filename specified to store the args
+    if [ -z "$ARGS_FILE" ]; then ARGS_FILE="$(realpath "array_$PROJECT.txt")" ; fi
+    # make sure the args file is absolute
+    case "$ARGS_FILE" in
+      /*) true ;;
+      *) echo "ARGS_FILE is not absolute, got: $ARGS_FILE" ; exit 1 ;;
+    esac
+    # submit everything
+    ARGS_FILE="$ARGS_FILE" bash "$SCRIPT_DIR/sbatch_submit.sh"
+}
+
+function submit_sbatch() {
+  gen_sbatch_file "$@"
+  submit_sbatch_file
 }
 
 # export
