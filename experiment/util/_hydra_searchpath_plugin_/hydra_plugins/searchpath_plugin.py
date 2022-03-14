6e3e25062f30f648c59555761895c717a2504bc9
Nathan Michlo <NathanJMichlo@gmail.com>
2022-03-14 12:18:41 +0200

    split out hydra main code from experiment so we can reuse it

--- experiment/util/_hydra_searchpath_plugin_/hydra_plugins/searchpath_plugin.py
+++ experiment/util/_hydra_searchpath_plugin_/hydra_plugins/searchpath_plugin.py
@@ -14,15 +14,20 @@ log = logging.getLogger(__name__)
 
 
 class DisentExperimentSearchPathPlugin(SearchPathPlugin):
+
     def manipulate_search_path(self, search_path: ConfigSearchPath) -> None:
+        from experiment.util.hydra_main import _DISENT_CONFIG_DIRS
         # find paths
         paths = [
             *os.environ.get('DISENT_CONFIGS_PREPEND', '').split(';'),
-            os.path.abspath(os.path.join(__file__, '../../../..', 'config')),
+            *_DISENT_CONFIG_DIRS,
             *os.environ.get('DISENT_CONFIGS_APPEND', '').split(';'),
         ]
+        # print information
+        log.info(f' [disent-search-path-plugin]: Activated hydra plugin: {self.__class__.__name__}')
+        log.info(f' [disent-search-path-plugin]: To register more search paths, adjust the `DISENT_CONFIGS_PREPEND` and `DISENT_CONFIGS_APPEND` environment variables!')
         # add paths
         for path in paths:
             if path:
-                log.info(f' [disent-search-path]: {path}')
+                log.info(f' [disent-search-path] - {repr(path)}')
                 search_path.append(provider='disent-searchpath-plugin', path=os.path.abspath(path))
