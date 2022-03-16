b61dd0a5ecb1fcfa654e952da5478da124ab04b9
Nathan Michlo <NathanJMichlo@gmail.com>
2022-03-17 00:35:41 +0200

    sweep

--- research/part01_data_overlap/e03_modified_loss_xysquares/submit_overlap_loss_learnt.sh
+++ research/part01_data_overlap/e03_modified_loss_xysquares/submit_overlap_loss_learnt.sh
@@ -25,6 +25,7 @@ export PARALLELISM=24
 # - this needs to before we source the helper file
 ARGS_FILE="$(realpath "$(dirname -- "${BASH_SOURCE[0]}")")/array_overlap_learnt_${PROJECT}.txt"
 ARGS_FILE_PARAMS="$(realpath "$(dirname -- "${BASH_SOURCE[0]}")")/array_overlap_learnt_${PROJECT}_PARAMS.txt"
+ARGS_FILE_TUNED="$(realpath "$(dirname -- "${BASH_SOURCE[0]}")")/array_overlap_learnt_${PROJECT}_TUNED.txt"
 
 # source the helper file
 source "$(dirname "$(dirname "$(dirname "$(realpath -s "$0")")")")/scripts/helper.sh"
@@ -62,28 +63,56 @@ source "$(dirname "$(dirname "$(dirname "$(realpath -s "$0")")")")/scripts/helpe
 
 # HPARAM TUNING FOR KERNEL
 # -- 1 * (2*4*2*2) = 32
-ARGS_FILE="$ARGS_FILE_PARAMS" gen_sbatch_args_file \
-    +DUMMY.repeat=1 \
-    +EXTRA.tags='sweep_overlap_learnt_hparams' \
-    hydra.job.name="hparam_tune" \
+#ARGS_FILE="$ARGS_FILE_PARAMS" gen_sbatch_args_file \
+#    +DUMMY.repeat=1 \
+#    +EXTRA.tags='sweep_overlap_learnt_hparams' \
+#    hydra.job.name="hparam_tune" \
+#    \
+#    run_length=medium \
+#    metrics=all \
+#    \
+#    dataset=X--xysquares \
+#    settings.framework.beta=0.0316,0.0001 \
+#    \
+#    +VAR.kernel_loss_weight=1.0,10.0,100.0,1000.0 \
+#    +VAR.kernel_norm_mode=sum,abssum \
+#    \
+#    settings.framework.recon_loss='mse_xy8_r47_l1.0_k${VAR.kernel_loss_weight}_norm_${VAR.kernel_norm_mode}' \
+#    \
+#    framework=adavae_os,betavae \
+#    settings.model.z_size=25 \
+#    \
+#    sampling=default__bb
+
+# RUN THE EXPERIMENT:
+#clog_cudaless_nodes "$PARTITION" 86400 "C-disent" # 24 hours
+#ARGS_FILE="$ARGS_FILE_PARAMS" submit_sbatch_args_file
+
+
+# TEST: MSE Learnt (USING ABOVE HPARAMS)
+# - EXTENDS: "TEST MSE vs BoxBlur MSE" from: "submit_overlap_loss.sh"
+# -- in plotting, combine the results with `EXTRA.tags=="sweep_overlap_boxblur_specific"`
+# 5 * (3*2*2) = 60
+ARGS_FILE="$ARGS_FILE_TUNED" gen_sbatch_args_file \
+    +DUMMY.repeat=1,2,3,4,5 \
+    +EXTRA.tags='sweep_overlap_learnt_TUNED' \
+    hydra.job.name="l_ovlp_loss" \
     \
     run_length=medium \
     metrics=all \
     \
     dataset=X--xysquares \
+    \
+    +VAR.kernel_loss_weight=100.0,300.0,1000.0 \
+    +VAR.kernel_norm_mode=sum \
+    \
+    framework=betavae,adavae_os \
     settings.framework.beta=0.0316,0.0001 \
-    \
-    +VAR.kernel_loss_weight=1.0,10.0,100.0,1000.0 \
-    +VAR.kernel_norm_mode=sum,abssum \
-    \
-    settings.framework.recon_loss='mse_xy8_r47_l1.0_k${VAR.kernel_loss_weight}_norm_${VAR.kernel_norm_mode}' \
-    \
-    framework=adavae_os,betavae \
     settings.model.z_size=25 \
+    settings.framework.recon_loss='mse_xy8_r47_l1.0_k${VAR.kernel_loss_weight}_norm_${VAR.kernel_norm_mode}' \
     \
     sampling=default__bb
 
-
 # RUN THE EXPERIMENT:
-clog_cudaless_nodes "$PARTITION" 86400 "C-disent" # 24 hours
-ARGS_FILE="$ARGS_FILE_PARAMS" submit_sbatch_args_file
+#clog_cudaless_nodes "$PARTITION" 86400 "C-disent" # 24 hours
+ARGS_FILE="$ARGS_FILE_TUNED" submit_sbatch_args_file
